a43bc17dd0d2e631ffb5a734c78f7137108ce2c2
Ishaan29 <bajpaiishan@yahoo.in>
2020-06-24 20:49:39 +0530

    new contest problems

--- codingblocks/math/marbals.cpp
+++ codingblocks/math/marbals.cpp
@@ -1,10 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+long long C(int n, int r) {
+    if(r > n - r) r = n - r; // because C(n, r) == C(n, n - r)
+    long long ans = 1;
+    int i;
 
+    for(i = 1; i <= r; i++) {
+        ans *= n - r + i;
+        ans /= i;
+    }
+
+    return ans;
+}
 int main(){
     int t; cin>>t;
     while(t--){
-        int a,b; cin>>a>>b;
-        
+        int n,k; cin>>n>>k;
+        cout<<C(n-1,k-1)<<endl;
     }
 }
